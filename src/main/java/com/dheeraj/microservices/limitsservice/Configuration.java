0410808170ea9d478afbf8c3f1313e1a2b047e99
DheerajKholia <dheraj.kholia@yahoo.com>
2020-01-20 07:38:15 +0530

    Updating to use spring cloud config

--- src/main/java/com/dheeraj/microservices/limitsservice/Configuration.java
+++ src/main/java/com/dheeraj/microservices/limitsservice/Configuration.java
@@ -3,7 +3,7 @@ package com.dheeraj.microservices.limitsservice;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @ConfigurationProperties("limits-service")
 public class Configuration {
     private int minimum;
